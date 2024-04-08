6c47ad4436afe671a45b47efbdb7f11649bbde81
Yvan Yang <ybyang@best-inc.com>
2024-04-08 17:13:15 +0800

    add pretty code for next/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
+import fs from "node:fs";
 import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,11 +11,22 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  keepBackground: false,
+  theme: JSON.parse(
+    fs.readFileSync(
+      new URL("./assets/moonlight-ii.json", import.meta.url),
+      "utf-8",
+    ),
+  ),
+};
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options], rehypeSlug],
   },
 });
 
